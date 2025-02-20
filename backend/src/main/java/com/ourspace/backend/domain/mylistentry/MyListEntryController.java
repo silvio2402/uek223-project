adf419c212f3f74d556b0870ceb6e9b21158d017
Battino <battino46@gmail.com>
2025-02-20 13:58:30 +0000

    Add remaining mappings for MyListEntity

--- backend/src/main/java/com/ourspace/backend/domain/mylistentry/MyListEntryController.java
+++ backend/src/main/java/com/ourspace/backend/domain/mylistentry/MyListEntryController.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,18 @@ public class MyListEntryController {
   public ResponseEntity<MyListEntry> create(@Valid @RequestBody MyListEntry myListEntry, User user) {
     MyListEntry createdMyListEntry = mylistentryService.create(myListEntry, user);
     return ResponseEntity.ok(createdMyListEntry);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<MyListEntry> update(@PathVariable UUID id, @Valid @RequestBody MyListEntry myListEntry) {
+    MyListEntry updatedMyListEntry = mylistentryService.updateById(id, myListEntry);
+    return ResponseEntity.ok(updatedMyListEntry);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable UUID id) {
+    mylistentryService.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 
 }
--- backend/src/main/java/com/ourspace/backend/domain/mylistentry/MyListEntryServiceImpl.java
+++ backend/src/main/java/com/ourspace/backend/domain/mylistentry/MyListEntryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ourspace.backend.domain.mylistentry;
 
+import org.springframework.stereotype.Service;
+
 import com.ourspace.backend.core.generic.AbstractRepository;
 import com.ourspace.backend.core.generic.AbstractServiceImpl;
 import com.ourspace.backend.domain.user.User;
 
+@Service
 public class MyListEntryServiceImpl extends AbstractServiceImpl<MyListEntry> implements MyListEntryService {
 
   public MyListEntryServiceImpl(AbstractRepository<MyListEntry> repository) {
