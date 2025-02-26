f98ce9d2a6460dd11943fa85345d0c9d20e171ac
Silvio Braendle <silvio@silviobraendle.ch>
2025-02-26 13:51:43 +0000

    Add user buttons to top bar

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,40 @@
+import { AppBar, Button, Toolbar, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+import { useActiveUser } from "../contexts/activeUser"
+import { logoutAuth } from "../services/authService"
+
+function TopBar() {
+  const user = useActiveUser()
+
+  return (
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        boxShadow: "none",
+        borderBottom: "2px solid #e0e0e0",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+            OurSpace
+          </Link>
+        </Typography>
+        {user === null && (
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+        )}
+        {user != null && (
+          <Button color="inherit" onClick={async () => logoutAuth()}>
+            Logout
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default TopBar
