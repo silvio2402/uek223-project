23ba511d81959710f5045789705fb1e8704904b8
Silvio Braendle <silvio@silviobraendle.ch>
2025-02-27 15:20:55 +0000

    Add links to home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
 import { Typography } from "@mui/material"
 import { Box } from "@mui/system"
+import { Link } from "react-router-dom"
+import { useActiveUser } from "../contexts/activeUser"
 
 function HomePage() {
+  const activeUser = useActiveUser()
+
   return (
     <Box>
       <Typography variant="h2" mt={4}>
         Welcome to OurSpace
       </Typography>
+      {activeUser && (
+        <>
+          <Link to="/mylistentries">My List Entries</Link>
+          <br />
+          <Link to="/users">Users</Link>
+        </>
+      )}
     </Box>
   )
 }
