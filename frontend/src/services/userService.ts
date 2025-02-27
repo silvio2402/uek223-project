327dba32abfbe9eab0ee49ff247085fa7c68b05e
Silvio Braendle <silvio@silviobraendle.ch>
2025-02-27 07:33:18 +0000

    Invalidate queries for user service and fix types

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,26 +1,30 @@
 import api from "../config/api"
 import { User } from "../types/models/User.model"
 import { UserRegister } from "../types/models/UserRegister.model"
+import { queryClient } from "./hooks/queryClient"
 
-export async function getUser(userID: string): Promise<User> {
+export async function getUser(userID: string) {
   const { data } = await api.get<User>(`/user/${userID}`)
   return data
 }
 
-export async function updateUser(user: User): Promise<void> {
+export async function updateUser(user: User) {
   await api.put(`/user/${user.id}`, user)
+  queryClient.invalidateQueries({ queryKey: ["users"] })
 }
 
-export async function addUser(user: UserRegister): Promise<User> {
-  const { data } = await api.post("/user/register", user)
+export async function addUser(user: UserRegister) {
+  const { data } = await api.post<User>("/user/register", user)
+  queryClient.invalidateQueries({ queryKey: ["users"] })
   return data
 }
 
-export async function getAllUsers(): Promise<User[]> {
+export async function getAllUsers() {
   const { data } = await api.get<User[]>(`/user`)
   return data
 }
 
-export async function deleteUser(id: string): Promise<void> {
+export async function deleteUser(id: string) {
   await api.delete(`/user/${id}`)
+  queryClient.invalidateQueries({ queryKey: ["users"] })
 }
