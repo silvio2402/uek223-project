997abf5bb8f34fbe2e1f4d0af43e441a62378013
Silvio Braendle <silvio@silviobraendle.ch>
2025-02-27 07:36:45 +0000

    Clean up home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,12 @@
+import { Typography } from "@mui/material"
 import { Box } from "@mui/system"
 
 function HomePage() {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      height="100vh"
-    >
-      <h1>OurSpace</h1>
+    <Box>
+      <Typography variant="h2" mt={4}>
+        Welcome to OurSpace
+      </Typography>
     </Box>
   )
 }
