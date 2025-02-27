1fbdff15b9297da60fdaca528d9629df7f827c9e
Silvio Braendle <silvio@silviobraendle.ch>
2025-02-27 12:25:39 +0000

    Add validation constraints

--- backend/src/main/java/com/ourspace/backend/domain/auth/dto/AuthRequest.java
+++ backend/src/main/java/com/ourspace/backend/domain/auth/dto/AuthRequest.java
@@ -1,5 +1,6 @@
 package com.ourspace.backend.domain.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,8 @@ import lombok.Setter;
 @Setter
 @Builder
 public class AuthRequest {
+  @NotBlank
   private String email;
+  @NotBlank
   private String password;
 }
